1c39da14e5d425fd599201b6601c94e7f5ced3ad
LuluNelle <mkhasibeluyanda@gmail.com>
2023-08-12 14:08:44 -0700

    Betty keeps giving me errors for 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h> 
+/**
+ * main - a programming that uses a prinf function to display code.
+ * Return: 0 (Success)
+ */
+int main(void)
+{ 
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
