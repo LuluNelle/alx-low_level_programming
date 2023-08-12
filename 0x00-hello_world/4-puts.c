86f89747fffff8b28d65bbdd609b8986d200192e
LuluNelle <mkhasibeluyanda@gmail.com>
2023-08-12 13:16:08 -0700

    done with 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+include <stdio.h>
+/**
+ * main - A program that displays a line using a puts function 
+ * Return: 0 (Success)
+ */
+int main(void)
+{	
+	puts("\"Programmming is like building a multilingual puzzle");
+	return(0);
+}
