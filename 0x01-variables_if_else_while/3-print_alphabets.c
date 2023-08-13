d03f80ab2f3ae3a3168bdeb021388ec6b330068e
LuluNelle <mkhasibeluyanda@gmail.com>
2023-08-13 02:37:15 -0700

    almost done

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,31 +1,24 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Description: Prints the alphabet in lowercase, followed by uppercase,
- *              and a new line, using only the putchar function.
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Description: Prints the alphabet in lowercase, followed by uppercase,
+*              and a new line, using only the putchar function.
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-    char letter;
-
-    /* Print lowercase alphabet */
-    for (letter = 'a'; letter <= 'z'; letter++)
-    {
-        putchar(letter);
-    }
-
-    /* Print uppercase alphabet */
-    for (letter = 'A'; letter <= 'Z'; letter++)
-    {
-        putchar(letter);
-    }
-
-    /* Print new line */
-    putchar('\n');
-
-    return (0);
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+putchar(letter);
+}
+for (letter = 'A'; letter <= 'Z'; letter++)
+{
+putchar(letter);
+}
+putchar('\n');
+return (0);
 }
--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,26 +1,23 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Description: Prints the alphabet in lowercase, excluding 'q' and 'e',
- *              followed by a new line, using only the putchar function.
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Description: Prints the alphabet in lowercase, excluding 'q' and 'e',
+*              followed by a new line, using only the putchar function.
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-    char letter;
-
-    for (letter = 'a'; letter <= 'z'; letter++)
-    {
-        if (letter != 'q' && letter != 'e')
-        {
-            putchar(letter);
-        }
-    }
-
-    putchar('\n'); /* Print a new line */
-
-    return (0);
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+if (letter != 'q' && letter != 'e')
+{
+putchar(letter);
+}
+}
+putchar('\n'); /* Print a new line */
+return (0);
 }
