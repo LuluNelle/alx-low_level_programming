d6049703ec24ca6cb91b5c430b9755504ec7daaf
LuluNelle <mkhasibeluyanda@gmail.com>
2023-08-13 01:27:17 -0700

    a + sign here converts the number to a charecter

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints the numbers from 0 to 9 using the putchar function.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    for (int i = 0; i < 10; i++)
+    int i;
+
+    for (i = 0; i < 10; i++)
     {
         putchar(i + '0');
     }
