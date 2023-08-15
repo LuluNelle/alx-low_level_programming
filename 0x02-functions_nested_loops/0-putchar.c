b4c2d42c9057dc416f8d1949a9456874bcf94d84
LuluNelle <mkhasibeluyanda@gmail.com>
2023-08-15 10:09:04 -0700

    First file attempt.

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/** 
+* main - Entry point 
+* Description: This code prints the word _putchar
+* Return: Always 0 (Success)
+*/
+
+int main()
+{
+putchar("putchar\n");
+return 0;
+}
